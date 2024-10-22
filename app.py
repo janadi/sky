30b2fd4ec2f4ca5f50ddc1d5b186a51b2c202f46
janadi <jhadolphs@gmail.com>
2024-10-22 22:50:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +8,6 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # Verwende den von Cloud Run bereitgestellten Port (standardmäßig 8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
